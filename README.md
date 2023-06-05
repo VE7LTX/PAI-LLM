efb4a90dd1077cf1311220688bcdb5cac1f2a17b
Matthew Schafer <131719047+VE7LTX@users.noreply.github.com>
2023-06-05 23:49:52 +0000

    Changes to be committed: new file:   .env_template 	modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PAI-LLM
 A LLM Huggingface Addon for Personal.ai
+
+
+## REMOVE THE _template on the end of the .env file!
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-import requests
-import json
-from getpass import getpass
+"""
+LangChain Chatbot Script
+
+This script demonstrates how to use the LangChain package to create a chatbot 
+that interacts with multiple language models sequentially and sends the 
+conversation history to the PERSONAL_AI memory stack. 
+
+The script follows these steps:
+
+1. Set up the necessary dependencies and API tokens.
+2. Define a list of model names to be used in the LangChain.
+3. Create LLMChain objects for each model name, initializing them with the HuggingFaceHub and PromptTemplate objects.
+4. Prompt the user for a question.
+5. Iterate through each model in the LLMChain and generate responses using LangChain.
+6. Send the user's question and the generated responses to the PERSONAL_AI Message API for further processing.
+7. Convert the conversation history and responses to a JSONL-formatted string.
+8. Create a memory with the conversation history and responses using the PERSONAL_AI Memory API.
+9. Print the result of creating the memory or handle any errors.
+
+Author: Matthew Schafer
+Date: 2023-June-05
+"""
+
+# Import necessary libraries and modules
 import os
 from datetime import datetime
+import json
 from langchain import HuggingFaceHub, LLMChain, PromptTemplate
+import requests
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Set up Hugging Face Hub LLM
-HUGGINGFACEHUB_API_TOKEN = getpass()
-PERSONAL_AI_API_KEY = getpass()
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
-os.environ["PERSONAL_AI_API_KEY"] = PERSONAL_AI_API_KEY
-
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+os.environ["PERSONAL_AI_API_KEY"] = os.getenv("PERSONAL_AI_API_KEY")
 
 # List of model names
 model_names = [
@@ -37,10 +62,10 @@ model_names = [
 # Loop through model names and create LLMChain for each
 llm_chains = {}
 for model_name in model_names:
-    hub_llm = HuggingFaceHub(repo_id=model_name, model_kwargs={"temperature": 0, "max_length": 64})
-    template = """User: {user_input}\nBot: """
-    prompt = PromptTemplate(template=template, input_variables=['user_input'])
-    llm_chains[model_name] = LLMChain(prompt=prompt, llm=hub_llm)
+    HUB_LLM = HuggingFaceHub(repo_id=model_name, model_kwargs={"temperature": 0, "max_length": 64})
+    TEMPLATE= """User: {user_input}\nBot: """
+    PROMPT = PromptTemplate(TEMPLATE=TEMPLATE, input_variables=['user_input'])
+    llm_chains[model_name] = LLMChain(PROMPT=PROMPT, llm=HUB_LLM)
 
 # Ask a user question
 user_question = input("Enter your question: ")
@@ -75,7 +100,8 @@ headers = {
     "Content-Type": "application/json",
     "x-api-key": PERSONAL_AI_API_KEY
 }
-response = requests.post(url, headers=headers, json=payload)
+print("Sending user question and responses to PERSONAL_AI Message API...")
+response = requests.post(url, headers=headers, json=payload, timeout=30)
 
 # Process the response from PERSONAL_AI Message API
 if response.status_code == 200:
@@ -88,29 +114,30 @@ else:
     print("Failed to send message to PERSONAL_AI:", response.text)
 
 # Convert conversation history and responses to JSONL-formatted string
-jsonl_string = ""
+JSONL_STRING = ""
 for response in responses:
     model_name, response_text = response
-    jsonl_string += json.dumps({
+    JSONL_STRING += json.dumps({
         "user_question": user_question,
         "model_name": model_name,
         "response": response_text
     }) + "\n"
 
 # Create a memory with the conversation history and responses
-memory_api_url = "https://api.personal.ai/v1/memory"
-memory_payload = {
-    "Text": jsonl_string,
+MEMORY_API_URL = "https://api.personal.ai/v1/memory"
+MEMORY_PAYLOAD = {
+    "Text": JSONL_STRING,
     "SourceName": "Conversation History",
     "CreatedTime": datetime.now().strftime("%a, %d %b %Y %H:%M:%S %Z"),
-    "DeviceName": "Macbook Pro"
+    "DeviceName": "Laptop"
 }
 
 # Make a POST request to create a memory with the conversation history and responses
+print("Creating memory with conversation history and responses...")
 response = requests.post(
-    memory_api_url,
+    MEMORY_API_URL,
     headers={"Content-Type": "application/json", "x-api-key": PERSONAL_AI_API_KEY},
-    json=memory_payload,
+    json=MEMORY_PAYLOAD,
     timeout=30  # Set the timeout to 30 seconds instead of the default 1
 )
 
